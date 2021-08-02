e8a2efb405b597ac35c30d784b903f4c4d92e857
Francisco Puma <fppuma@gmail.com>
2021-08-01 23:09:46 -0500

    update replaceEmployee()

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -71,9 +71,9 @@ public class EmployeeController {
 	}
 
 	@PutMapping("/employees/{id}")
-	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
+	ResponseEntity<?> replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
 
-		return repository.findById(id).map(employee -> {
+		Employee updatedEmployee =  repository.findById(id).map(employee -> {
 			employee.setName(newEmployee.getName());
 			employee.setRole(newEmployee.getRole());
 			return repository.save(employee);
@@ -81,6 +81,12 @@ public class EmployeeController {
 			newEmployee.setId(id);
 			return repository.save(newEmployee);
 		});
+		
+		EntityModel<Employee> entityModel = assembler.toModel(updatedEmployee);
+
+		  return ResponseEntity //
+		      .created(entityModel.getRequiredLink(IanaLinkRelations.SELF).toUri()) //
+		      .body(entityModel);
 	}
 
 	@DeleteMapping("/employees/{id}")
