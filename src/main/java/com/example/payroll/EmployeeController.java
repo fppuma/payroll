00c327086137c741046970584d9e3e78b38fc145
Francisco Puma <fppuma@gmail.com>
2021-08-01 21:53:03 -0500

    return.type from Employee to EntityModel<Employee>

--- src/main/java/com/example/payroll/EmployeeController.java
+++ src/main/java/com/example/payroll/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.payroll;
 
 import java.util.List;
 
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 @RestController
 public class EmployeeController {
 	private final EmployeeRepository repository;
@@ -39,10 +40,15 @@ public class EmployeeController {
 	 * @return entity
 	 */
 	@GetMapping("/employees/{id}")
-	Employee one(@PathVariable Long id) {
+	EntityModel<Employee> one(@PathVariable Long id) {
 
-		return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
-	}
+		  Employee employee = repository.findById(id) //
+		      .orElseThrow(() -> new EmployeeNotFoundException(id));
+
+		  return EntityModel.of(employee, //
+		      linkTo(methodOn(EmployeeController.class).one(id)).withSelfRel(),
+		      linkTo(methodOn(EmployeeController.class).all()).withRel("employees"));
+		}
 
 	@PutMapping("/employees/{id}")
 	Employee replaceEmployee(@RequestBody Employee newEmployee, @PathVariable Long id) {
