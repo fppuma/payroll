6ec762e26b305e6ed99d863d760f7fbf0f21660a
Francisco Puma <fppuma@gmail.com>
2021-08-01 22:59:00 -0500

    change name to firstName and lastName

--- src/main/java/com/example/payroll/Employee.java
+++ src/main/java/com/example/payroll/Employee.java
@@ -9,15 +9,18 @@ import javax.persistence.Id;
 @Entity
 public class Employee {
 	private @Id @GeneratedValue Long id;
-	private String name;
+	private String firstName;
+	private String lastName;
+
 	private String role;
 
 	Employee() {
 	}
 
-	Employee(String name, String role) {
+	Employee(String firstName, String lastName, String role) {
 
-		this.name = name;
+		this.firstName = firstName;
+		this.lastName = lastName;
 		this.role = role;
 	}
 
@@ -26,7 +29,7 @@ public class Employee {
 	}
 
 	public String getName() {
-		return this.name;
+		return this.firstName + " " + this.lastName;
 	}
 
 	public String getRole() {
@@ -38,7 +41,9 @@ public class Employee {
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		String[] parts = name.split(" ");
+		this.firstName = parts[0];
+		this.lastName = parts[1];
 	}
 
 	public void setRole(String role) {
@@ -53,17 +58,35 @@ public class Employee {
 		if (!(o instanceof Employee))
 			return false;
 		Employee employee = (Employee) o;
-		return Objects.equals(this.id, employee.id) && Objects.equals(this.name, employee.name)
-				&& Objects.equals(this.role, employee.role);
+		return Objects.equals(this.id, employee.id) && Objects.equals(this.firstName, employee.firstName)
+				&& Objects.equals(this.lastName, employee.lastName) && Objects.equals(this.role, employee.role);
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(this.id, this.name, this.role);
+		return Objects.hash(this.id, this.firstName, this.lastName, this.role);
 	}
 
 	@Override
 	public String toString() {
-		return "Employee{" + "id=" + this.id + ", name='" + this.name + '\'' + ", role='" + this.role + '\'' + '}';
+		return "Employee{" + "id=" + this.id + ", firstName='" + this.firstName + '\'' + ", lastName='" + this.lastName
+				+ '\'' + ", role='" + this.role + '\'' + '}';
 	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 }
