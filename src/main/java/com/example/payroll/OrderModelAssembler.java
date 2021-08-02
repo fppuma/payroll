2a08f67ed0add45eb82cf6cb030f172910690fbe
Francisco Puma <fppuma@gmail.com>
2021-08-01 23:54:36 -0500

    add links for in_progress status

--- src/main/java/com/example/payroll/OrderModelAssembler.java
+++ src/main/java/com/example/payroll/OrderModelAssembler.java
@@ -7,17 +7,22 @@ import org.springframework.stereotype.Component;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @Component
-public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>>{
+public class OrderModelAssembler implements RepresentationModelAssembler<Order, EntityModel<Order>> {
 
 	@Override
 	public EntityModel<Order> toModel(Order order) {
 		// Unconditional links to single-item resource and aggregate root
 
-	    EntityModel<Order> orderModel = EntityModel.of(order,
-	        linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
-	        linkTo(methodOn(OrderController.class).all()).withRel("orders"));
+		EntityModel<Order> orderModel = EntityModel.of(order,
+				linkTo(methodOn(OrderController.class).one(order.getId())).withSelfRel(),
+				linkTo(methodOn(OrderController.class).all()).withRel("orders"));
 
-	    return orderModel;
+		if (order.getStatus() == Status.IN_PROGRESS) {
+			orderModel.add(linkTo(methodOn(OrderController.class).cancel(order.getId())).withRel("cancel"));
+			orderModel.add(linkTo(methodOn(OrderController.class).complete(order.getId())).withRel("complete"));
+		}
+
+		return orderModel;
 	}
 
 }
